b4f8d96f8cea8fa9f4a667f5728a3e699593cb80
taufan <hadikusumaa15@gmail.com>
2019-07-23 12:25:26 +0700

    question

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -1,27 +1,2 @@
 class ComputerController < ApplicationController
-    def initialize()
-        @data = []
-        @size = 0
-    end
-
-    def play(input=nil)
-        return input
-    end
-
-    def size(data=[])
-        return @size
-    end
-
-    def store(input)
-        @data.push(input)
-        @size = @size + 1
-        return @data
-    end
-
-    def delete(input)
-        @data = @data - [input]
-        @size = @size - 1
-        return @data
-    end
-
 end
