fbd657bd7c668ae7c8e2764f85b0ef7cbde776d4
taufan <hadikusumaa15@gmail.com>
2019-07-23 13:32:14 +0700

    notref

--- app/controllers/computer_controller.rb
+++ app/controllers/computer_controller.rb
@@ -2,13 +2,14 @@ class ComputerController < ApplicationController
     def initialize()
         @data = []
         @size = 0
+        @number = 0
     end
 
     def play(input=nil)
         return input
     end
 
-    def size(data=[])
+    def size(data)
         return @size
     end
 
@@ -22,6 +23,26 @@ class ComputerController < ApplicationController
         @data = @data - [input]
         @size = @size - 1
         return @data
+    end
+
+    def add_numbers(arr)
+        arr.each do |num|
+            @number = @number + num
+        end
+        return @number
+    end
+
+    def substract_numbers(numbers_a, numbers_b)
+        total_a = 0
+        total_b = 0
+        numbers_a.each do |a|
+            total_a = total_a + a
+        end
+
+        numbers_b.each do |b|
+            total_b = total_b + b
+        end
+        return total_a - total_b
     end
 
 end
--- test/controllers/computer_controller_test.rb
+++ test/controllers/computer_controller_test.rb
@@ -4,20 +4,22 @@ require 'colorize'
 class ComputerControllerTest < ActionDispatch::IntegrationTest
   success = "test success"
   failed = "test failed"
-  computer = ComputerController.new()
+
   nil_value = "Computer can return nil value"
   value = "Computer can return value"
   data_size = "Computer can calculate data size"
   data_storage = "Computer can store array data"
   data_delete = "Computer can delete stored array data"
-
-
+  add_up_numbers = "Computer can add up numbers"
+  substract_numbers = "Computer can substract numbers with numbers"
+  
   def message(status="", message="")
     return "#{status}, #{message}"
   end
 
   test nil_value do
     begin
+      computer = ComputerController.new()
       return_nil = computer.play()
       assert_nil return_nil
       puts message(success, nil_value).green
@@ -28,6 +30,7 @@ class ComputerControllerTest < ActionDispatch::IntegrationTest
 
   test value do 
     begin
+      computer = ComputerController.new()
       return_value = computer.play(10)
       assert_equal 10, return_value
       puts message(success, value).green
@@ -38,10 +41,11 @@ class ComputerControllerTest < ActionDispatch::IntegrationTest
 
   test data_storage do 
     begin
-      storage = computer.store(1)
-      storage = computer.store(2)
-      storage = computer.store(3)
-      assert_equal [1,2,3], storage
+      computer = ComputerController.new()
+      computer.store(1)
+      computer.store(2)
+      stored_data = computer.store(3)
+      assert_equal [1,2,3], stored_data
       puts message(success, data_storage).green
     rescue => err
       puts message(failed, err.to_s).red
@@ -50,11 +54,12 @@ class ComputerControllerTest < ActionDispatch::IntegrationTest
 
   test data_delete do 
     begin
-      storage = computer.store(1)
-      storage = computer.store(2)
-      storage = computer.store(3)
-      storage = computer.delete(2)
-      assert_equal [1,3], storage
+      computer = ComputerController.new()
+      computer.store(1)
+      computer.store(2)
+      computer.store(3)
+      delete = computer.delete(2)
+      assert_equal [1,3], delete
       puts message(success, data_delete).green
     rescue => err
       puts message(failed, err.to_s).red
@@ -63,8 +68,9 @@ class ComputerControllerTest < ActionDispatch::IntegrationTest
 
   test data_size do 
     begin
-      storage = computer.store(1)
-      storage = computer.store(2)
+      computer = ComputerController.new()
+      computer.store(1)
+      computer.store(2)
       storage = computer.store(3)
       size = computer.size(storage)
       assert_equal 3, size
@@ -73,5 +79,32 @@ class ComputerControllerTest < ActionDispatch::IntegrationTest
       puts message(failed, err.to_s).red
     end
   end
+
+  test add_up_numbers do 
+    begin
+      computer = ComputerController.new()
+      numbers = [2,3]
+      number = computer.add_numbers(numbers)
+      assert_equal 5, number
+      puts message(success, add_up_numbers).green
+    rescue => err
+      puts message(failed, err.to_s).red
+    end
+  end
+
+  test substract_numbers do 
+    begin
+      computer = ComputerController.new()
+      numbers_a = [9,3]
+      numbers_b = [1,2]
+      number = computer.substract_numbers(numbers_a, numbers_b)
+      assert_equal 9, number
+      puts message(success, substract_numbers).green
+    rescue => err
+      puts message(failed, err.to_s).red
+    end
+  end
+
+
 
 end
